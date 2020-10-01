eab74990331e932479a36db505b00841fd88e767
Terence21 <terencepeterson21@gmail.com>
2020-10-01 07:51:09 -0700

    created a color adapter and created gridView in activity main

--- app/src/main/java/temple/edu/gridassignment/PaletteActivity.java
+++ app/src/main/java/temple/edu/gridassignment/PaletteActivity.java
@@ -1,7 +1,11 @@
 package temple.edu.gridassignment;
 
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class PaletteActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class PaletteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setTitle(R.string.name_Palette_activity);
+
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("green");
+        colors.add("blue");
+        colors.add("red");
+        colors.add("yellow");
+        colors.add("teal");
+        colors.add("cyan");
+
+        GridView grid = findViewById(R.id._ColorGrid);
+        grid.setColumnWidth(3);
+
+        BaseAdapter adapter = new ColorAdapter(this,colors);
+        grid.setAdapter(adapter);
     }
 }
